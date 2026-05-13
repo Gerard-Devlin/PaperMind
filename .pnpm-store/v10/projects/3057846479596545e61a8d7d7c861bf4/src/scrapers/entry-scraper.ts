95201e4a7c9cbe27c92f4f611d12c6f5fb5b9956
Gerard-Devlin <alwenxuyouwen@gmail.com>
2026-05-14 02:01:51 +0800

    Remove TypeScript configuration and Vite build configuration files

--- .pnpm-store/v10/projects/3057846479596545e61a8d7d7c861bf4/src/scrapers/entry-scraper.ts
+++
@@ -1,6 +0,0 @@
-import { PaperEntity } from "paperlib-api/model";
-
-export abstract class AbstractEntryScraper {
-  static validPayload: (payload: any) => boolean;
-  static scrape: (payload: any) => Promise<PaperEntity[]>;
-}
